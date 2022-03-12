15fa59e5f5180a9770393eddc39332428ed17a31
Alex Nordhoff <73557166+alexnordhoff@users.noreply.github.com>
2022-03-11 23:52:26 -0600

    working on stuff

--- src/components/gamepage.js
+++ src/components/gamepage.js
@@ -1,31 +1,79 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 
-import { Col, Container, Row, Modal, Button } from 'react-bootstrap'
+import { Col, Container, Row, Modal, Button, Form } from 'react-bootstrap'
 import {BsQuestionCircle, BsBarChart} from 'react-icons/bs'
 
 const GamePage = () => {
     // let navigate = useNavigate(); // For router navigation
-    
+    const [firstGuess, setfirstGuess] = useState("");
+    const [secondGuess, setsecondGuess] = useState("");
+    const [thirdGuess, setthirdGuess] = useState("");
+    const [fourthGuess, setfourthGuess] = useState("");
+    const [fifthGuess, setfifthGuess] = useState("");
+    const [sixthGuess, setsixthGuess] = useState("");
  
     return (
-        <Container style={{color: 'white'}}>
+        <Container style={{color: 'white', fontSize: "3vh"}}>
             <hr />
-            <Row>
-                <Col />
-                <Col>
-                    This is a row.
-                </Col>
-                <Col />
-                
-            </Row>
-            <Row>
-                <Col />
-                <Col>
-                    This is another row. 
-                </Col>
+            <Row style={{paddingBottom: '2vh'}}>
                 <Col />
 
+                <Col xs={5} className="block-example border border-white rounded" style={{minHeight : '4.8vh', backgroundColor: '#323545'}}>
+                    {firstGuess}
+                </Col>
+
+                <Col />
+            </Row>
+
+            <Row style={{paddingBottom: '2vh'}}>
+                <Col />
+
+                <Col xs={5} className="block-example border border-white rounded" style={{minHeight : '4.8vh', backgroundColor: '#323545'}}>
+                    {secondGuess}
+                </Col>
+
+                <Col />
+            </Row>
+
+            <Row style={{paddingBottom: '2vh'}}>
+                <Col />
+
+                <Col xs={5} className="block-example border border-white rounded" style={{minHeight : '4.8vh', backgroundColor: '#323545'}}>
+                    {thirdGuess}
+                </Col>
+
+                <Col />
+            </Row>
+
+            <Row style={{paddingBottom: '2vh'}}>
+                <Col />
+
+                <Col xs={5} className="block-example border border-white rounded" style={{minHeight : '4.8vh', backgroundColor: '#323545'}}>
+                    {fourthGuess}
+                </Col>
+
+                <Col />
+            </Row>
+
+            <Row style={{paddingBottom: '2vh'}}>
+                <Col />
+
+                <Col xs={5} className="block-example border border-white rounded" style={{minHeight : '4.8vh', backgroundColor: '#323545'}}>
+                    {fifthGuess}
+                </Col>
+
+                <Col />
+            </Row>
+
+            <Row style={{paddingBottom: '2vh'}}>
+                <Col />
+
+                <Col xs={5} className="block-example border border-white rounded" style={{minHeight : '4.8vh', backgroundColor: '#323545'}}>
+                    {sixthGuess}
+                </Col>
+
+                <Col />
             </Row>
         </Container>
        
